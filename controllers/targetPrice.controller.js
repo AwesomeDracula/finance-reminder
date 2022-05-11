d20c22b501da029b53fe36f9ffb4164164490217
Trung Vu <trung.vu@jmango360.com>
2022-05-11 14:42:14 +0700

    Add function to handle when to send notification

--- controllers/targetPrice.controller.js
+++ controllers/targetPrice.controller.js
@@ -1,8 +1,8 @@
-const targetPriceService = require('../services/targetPrice.service');
+const targetPriceService = require("../services/targetPrice.service");
 
 async function create(req, res, next) {
   try {
-    const { symbol, tp, sl } = req.body;
+    const {symbol, tp, sl} = req.body;
     if (!symbol || !tp || !sl) {
       res.status(400).send("Missing required fields");
     }
@@ -10,7 +10,12 @@ async function create(req, res, next) {
       res.status(400).send("Take profit and stop loss must be numbers");
     }
     const data = await targetPriceService.create(symbol, tp, sl);
-    res.send(data);
+    console.log(data);
+    if (data) {
+      res.status(204).send(data);
+    } else {
+      res.status(400).send("Failed");
+    }
   } catch (err) {
     console.error(`Error while creating target price`, err.message);
     next(err);
--- services/targetPrice.service.js
+++ services/targetPrice.service.js
@@ -1,10 +1,46 @@
-const symbolsService = require('./symbols.service');
+const symbolsService = require("./symbols.service");
+const _ = require("lodash");
 
 async function create(symbol, tp, sl) {
-  const symbolIntradayData = await symbolsService.getSymbolIntraday(symbol);
-  console.log(symbolIntradayData);
+  try {
+    const symbolIntradayData = await symbolsService.getSymbolIntraday(symbol);
+    const metaData = symbolIntradayData["Meta Data"];
+    const latestDay = metaData["3. Last Refreshed"].substring(0, 10);
+    const series = symbolIntradayData["Time Series (1min)"];
+    const latestData = _.flow([
+      Object.entries,
+      (arr) => arr.filter(([key, value]) => key.includes(latestDay)),
+      Object.fromEntries,
+    ])(series);
+    const timer = setInterval(function () {
+      const currentDate = new Date();
+      const key =
+        latestDay +
+        " " +
+        currentDate.getHours() +
+        ":" +
+        currentDate.getMinutes() +
+        ":00";
+        console.log(key);
+      const data = latestData[key];
+      if (tp <= parseFloat(data["4. close"])) {
+        console.log("TP");
+        clearInterval(timer);
+      } else if (sl >= parseFloat(data["4. close"])) {
+        console.log("SL");
+        clearInterval(timer);
+      } else {
+        console.log("Nothing");
+      }
+    }, 60000);
+    return latestData;
+  } catch (err) {
+    return false;
+  }
 }
+
+function intervalFunc(date, latestData, tp, sl) {}
 
 module.exports = {
   create,
-}
+};
